92c2fa86a7eddfe9d595c18bf4bf043fe7fdab73
THEDAN320 <103934625+THEDAN320@users.noreply.github.com>
2023-04-04 08:53:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ class apps(QMainWindow):
             
         self.des.encode(self.ui.text_input_edit.toPlainText().encode("utf-8"),self.ui.password_edit_1.text().encode("utf-8"))
         self.ui.code_output_edit.setText(self.des.m_code)
+        string = "code - "+ self.des.m_code+ "\npassword - "+ _DES.sha256(self.ui.password_edit_1.text().encode("utf-8"))+"\n"
+        with open("output.txt","w") as f:
+            f.write(string)
         
     def decode(self):
         if len(self.ui.code_input_edit.toPlainText()) < 1 or len(self.ui.password_edit_2.text()) < 1:
